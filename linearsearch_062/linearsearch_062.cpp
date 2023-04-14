d3fae09df51ee2e94fa31e3d6d646df0fd1359a8
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-04-14 15:10:51 +0700

    void inpu eccept element array

--- linearsearch_062/linearsearch_062.cpp
+++ linearsearch_062/linearsearch_062.cpp
@@ -14,3 +14,14 @@ void input() {
 		else
 			cout << "\narray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	//accept array elements
+	cout << "\n----------------------\n";
+	cout << " enter array elements \n";
+	cout << "------------------------";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
